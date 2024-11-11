96d7cb26d60b48909c937756c4703a97888aa921
Igor Knop <igor.knop@ufjf.br>
2024-11-11 20:35:52 -0300

    adiciona estilo

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,8 +1,12 @@
+import "./Disciplina.css";
+
 function Disciplina() {
-  return <div>
-    <div>DCC207</div>
-    <div>Desenvolvimento Web Front-End</div>
-  </div>;
+  return (
+    <div className="disciplina">
+      <div>DCC207</div>
+      <div>Desenvolvimento Web Front-End</div>
+    </div>
+  );
 }
 
 export default Disciplina;
