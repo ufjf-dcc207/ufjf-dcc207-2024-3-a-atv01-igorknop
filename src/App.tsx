85bcb20818c6b7e4f97160ce81f53e8dc33f0f50
Igor Knop <igor.knop@ufjf.br>
2024-11-11 20:48:22 -0300

    adiciona props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ export default function App() {
   return   (
     <>
       <h1>Olá mundo!</h1>
-      <Disciplina />
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I"/>
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
     </>
   );
 }
--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,13 +1,15 @@
 import "./Disciplina.css";
 
-function Disciplina() {
-    const codigo = "DCC207";
-    const nome = "Desenvolvimento Web Front-End";
+type DisciplinaProps = {
+    codigo: string;
+    nome: string;
+}
 
+function Disciplina(props: DisciplinaProps) {
   return (
     <div className="disciplina">
-      <div>{codigo}</div>
-      <div>{nome}</div>
+      <div>{props.codigo}</div>
+      <div>{props.nome}</div>
     </div>
   );
 }
