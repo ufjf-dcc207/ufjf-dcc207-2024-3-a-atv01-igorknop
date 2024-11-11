3f627eb06644e47bb127c8cf3bbf32655be33e9e
Igor Knop <igor.knop@ufjf.br>
2024-11-11 20:40:13 -0300

    extrai constantes

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,10 +1,13 @@
 import "./Disciplina.css";
 
 function Disciplina() {
+    const codigo = "DCC207";
+    const nome = "Desenvolvimento Web Front-End";
+
   return (
     <div className="disciplina">
-      <div>DCC207</div>
-      <div>Desenvolvimento Web Front-End</div>
+      <div>{codigo}</div>
+      <div>{nome}</div>
     </div>
   );
 }
